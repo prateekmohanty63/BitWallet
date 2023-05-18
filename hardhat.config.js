4066e8549554957f72a3c5780c1afbf739fc32f9
Prateek <prateekmohanty63@gmail.com>
2023-05-18 20:43:37 +0530

    Deployment sucessful on goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config();
+
+const GOERLI_URL=process.env.GOERLI_URL;
+const PRIVATE_KEY=process.env.PRIVATE_KEY;
+
+// /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks:{
+    goerli:{
+      url:GOERLI_URL,
+      accounts:[PRIVATE_KEY],
+    },
+  },
 };
