0f80c5f3034d6b8b92b877b3e8d65b25a6b3916d
Prateek <prateekmohanty63@gmail.com>
2023-05-19 17:03:09 +0530

    Feat:wallet connection done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,42 @@
-
+import abi from "./contract/chai.json"
 import {useState,useEffect} from 'react';
 import {ethers} from "ethers";
 import './App.css';
 
 function App() {
+
+   const [state,setState]=useState({
+    provider:null,
+    signer:null,
+    contract:null
+   })
+
+   useEffect(()=>{
+    const connectWallet=async()=>{
+       const contractAddress="0xdf3362f359508924e9E5aAa22c535b90765d032E"
+       const contractABI=abi.abi
+
+       try{
+        const {ethereum}=window;
+
+        if(ethereum){
+          const account=await ethereum.request({method:"eth_requestAccounts"})
+        }
+        const provider=new ethers.providers.Web3Provider(ethereum);
+        const signer=provider.getSigner();
+        const contract=new ethers.Contract(contractAddress,contractABI);
+        setState({provider,signer,contract})
+       }
+       catch(error){
+          console.log(error)
+       }
+
+    };
+    connectWallet();
+   },[])
+   console.log(state)
   return (
     <div className="App">
-      
     </div>
   );
 }
